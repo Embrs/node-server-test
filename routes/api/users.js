1b30f2e33c9c833c2cd71c698bf2ab7f95158ed2
鄒豐懋 <zoufengmao@zoufengmaodeMacBook-Pro.local>
2023-06-28 12:51:56 +0800

    backup

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,6 +74,11 @@ router.post("/login", (req,res) => {
 // @desc user info
 // @access Private
 router.get("/current", passport.authenticate("jwt", {session: false}), (req, res) => {
-  res.json(req.user)
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email,
+    
+  })
 })
 module.exports = router;
