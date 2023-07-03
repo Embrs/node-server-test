c73e1752799503b9cf21e1d5ce41a9a498c24ec2
Embers <ddembrs@gmail.com>
2023-07-03 17:31:10 +0800

    nodemon

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ require("./config/passport")(passport)
 // 使用 routers users
 app.use("/api/users", users)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 })
