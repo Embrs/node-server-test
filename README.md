58ff593b0396c7951b4be1f231d4c770b685ac30
AthenaHarry <harry@athenaads.com>
2024-07-17 15:02:33 +0800

    Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+### Node + ts + Eslint + Mongodb 伺服器測試
+https://www.notion.so/Node-ts-6b1236773f4d446ba49d5ba0ee33880b
--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoURI: 'mongodb+srv://ddembrs:hsXULQkuZi5BRdHK@restful-api.e6n7cuz.mongodb.net/?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://account:pwd@restful-api.e6n7cuz.mongodb.net/?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
